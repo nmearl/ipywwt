3de0e94dd5a550d1b2d85261b4e643ec8f4519dd
Nicholas Earl <contact@nicholasearl.me>
2023-03-08 19:21:36 -0600

    Setup vite build configuration

---
+++ src/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+export default defineConfig({
+    plugins: [viteSingleFile()],
+    build: {
+        outDir: "ipywwt/static",
+        lib: {
+            entry: ["js/index.js"],
+            formats: ["es"],
+        },
+    },
+});
