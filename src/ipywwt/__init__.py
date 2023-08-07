73c2923b8fd054c2995a240a7e3b3cf2d31128b2
Nicholas Earl <contact@nicholasearl.me>
2023-08-07 10:51:55 -0500

    Include built js and css files

--- src/ipywwt/__init__.py
+++ src/ipywwt/__init__.py
@@ -1,16 +1,13 @@
-import sys
+from typing import Any, Callable
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from anywidget import AnyWidget
+from pathlib import Path
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+bundler_output_dir = Path(__file__).parent / "static"
+
+
+class WWTWidget(AnyWidget):
+    _esm = bundler_output_dir / "index.js"
+    _css = bundler_output_dir / "style.css"
+
+    # attributes = traitlets.Dict(default_value=None, allow_none=True).tag(sync=True)
