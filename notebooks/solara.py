e38d5ceaac8520e731928a4dabb8e2b2ba85bb99
Nicholas Earl <contact@nicholasearl.me>
2023-08-09 13:54:19 -0500

    Add example solara usage

---
+++ notebooks/solara.py
@@ -0,0 +1,25 @@
+"""
+Example usage of ipywwt in a solara app. Run with:
+    solara run notebooks/solara.py
+"""
+import solara
+from ipywwt import WWTWidget
+import ipywidgets as w
+
+
+# @solara.component
+# def Page():
+#     with solara.Card() as col:
+#         slider = solara.SliderInt("Word limit", value=100, min=2, max=20)
+#
+#         with w.VBox() as vbox:
+#             wwt = WWTWidget()
+#             button = w.Button(description="Clicked 0 times")
+#
+#         # w.VBox([wwt, button])
+#
+#     return col
+
+wwt = WWTWidget()
+button = w.Button(description="Clicked 0 times")
+page = w.VBox([wwt, button])
