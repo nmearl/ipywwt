bdd7d95594d5144b3c96b166e67b94c07352ffed
Nicholas Earl <contact@nicholasearl.me>
2024-01-03 14:32:08 -0600

    Cleanup model sending handling

--- src/src/createRender.js
+++ src/src/createRender.js
@@ -1,6 +1,5 @@
 import * as wwtlib from "@wwtelescope/engine";
 import { classicPywwt } from "@wwtelescope/research-app-messages";
-import {CenterOnCoordinatesMessage} from "@wwtelescope/research-app-messages/dist/src/classic_pywwt.js";
 
 const ReferenceFramesRadius = {
     Sky: 149500000000,
@@ -26,13 +25,13 @@ export function createRender(app) {
         // let _appUrl = location.toString();
         // let _appOrigin = _appUrl.origin;
         //
-        // var iframe = document.createElement('iframe');
+        // let iframe = document.createElement('iframe');
         // // Pass our origin so that the iframe can validate the provenance of the
         // // messages that are posted to it. This isn't acceptable for real XSS
         // // prevention, but so long as the research app can't do anything on behalf
         // // of the user (which it can't right now because we don't even have
         // // "users"), that's OK.
-        // iframe.src = _appUrl + '?origin=' + encodeURIComponent(location.origin);
+        // // iframe.src = _appUrl + '?origin=' + encodeURIComponent(location.origin);
         // iframe.style.setProperty('height', '400px', '');
         // iframe.style.setProperty('width', '100%', '');
         // iframe.style.setProperty('border', 'none', '');
@@ -44,6 +43,8 @@ export function createRender(app) {
         div.style.setProperty('width', '100%', '');
         div.style.setProperty('border', 'none', '');
 
+        // iframe.contentWindow.document.body.appendChild(div);
+
         el.appendChild(div);
 
         let vm = app.mount(div);
@@ -52,13 +53,10 @@ export function createRender(app) {
 
         vm.layers = {};
 
-        // TODO: This should be working...
-        model.send(JSON.stringify({test: "TESTING"}));
+        model.send("ping");
 
         // Setup custom message handling
         model.on("msg:custom", (msg) => {
-            console.log(msg)
-
             let layerId = null;
             let proxyLayer = null;
             let layer = null;
@@ -66,34 +64,21 @@ export function createRender(app) {
 
             switch (msg.event) {
                 case "center_on_coordinates":
-                    event = new CustomEvent("message", {bubbles: true, detail: msg});
-
                     console.log(classicPywwt.isCenterOnCoordinatesMessage(msg));
-
-                    // vm.onMessage(msg);
-
-                    // window.dispatchEvent(event);
                     window.postMessage(msg);
                     break;
                 case "table_layer_create":
-                    event = new CustomEvent("message", {bubbles: true, detail: msg});
-
-                    console.log(classicPywwt.isCreateTableLayerMessage(msg));
-
-                    // vm.onMessage(msg);
-
-                    // window.dispatchEvent(event);
-
                     window.postMessage(msg);
-
                     break;
                 case "table_layer_update":
-                    [layerId, proxyLayer] = Object.entries(vm.wwtSpreadSheetLayers).filter( ([key, item]) => item.name === msg['id']).at(0);
-                    layer = vm.spreadSheetLayerById(layerId);
+                    // [layerId, proxyLayer] = Object.entries(vm.wwtSpreadSheetLayers).filter( ([key, item]) => item.name === msg['id']).at(0);
+                    // layer = vm.spreadSheetLayerById(layerId);
+                    //
+                    // vm.updateTableLayer({
+                    //     'dataCsv': atob(msg['table']),
+                    //     'id': layer.id.toString()})
 
-                    vm.updateTableLayer({
-                        'dataCsv': atob(msg['table']),
-                        'id': layer.id.toString()})
+                    window.postMessage(msg);
                     break;
                 case "table_layer_set":
                     [layerId, proxyLayer] = Object.entries(vm.wwtSpreadSheetLayers).filter( ([key, item]) => item.name === msg['id']).at(0);
@@ -128,25 +113,23 @@ export function createRender(app) {
                     }
 
                     msg['value'] = value;
-                    // msg['id'] = layerId;
-                    // vm.onMessage(msg)
                     window.postMessage(msg);
                     break;
                 case 'table_layer_remove':
                     layer = vm.layers[msg['id']];
-                    vm.deleteLayer(layer.id);
+                    window.postMessage(msg);
                     break;
                 case "load_image_collection":
-                    vm.loadImageCollection(msg['url'])
+                    window.postMessage(msg);
                     break;
                 case "set_foreground_by_name":
-                    vm.setForegroundImageByName(msg['name'])
+                    window.postMessage(msg);
                     break;
                 case "set_background_by_name":
-                    vm.setBackgroundImageByName(msg['name'])
+                    window.postMessage(msg);
                     break;
                 case "set_foreground_opacity":
-                    vm.setForegroundOpacity(msg['value'])
+                    window.postMessage(msg);
                     break;
                 default:
                     console.log(`Received uncaught custom message of type ${msg.event}.`)
@@ -157,9 +140,6 @@ export function createRender(app) {
         window.addEventListener(
             "message",
             (event) => {
-                // model.send(event.data);
-                console.log("SENDING EVENT TO PYTHON");
-                console.log(event.data);
                 model.send(event.data);
             }, false);
 
